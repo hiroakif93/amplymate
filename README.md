cf99bc3c19475c10e9daf61c2de0da4817bc2c77
hiroakif <hiroakif1637@gmail.com>
2026-06-19 10:04:00 +0900

    update

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Amplicon analysis mate
 This repository provide a pipeline analysing Miseq/Miseq i100 outputs.
+**This repo is STILL in DEVELOPMENT**
 
 ## Requirements
 - [Docker](https://docs.docker.com/engine/install/ubuntu/)
@@ -11,4 +12,24 @@ This repository provide a pipeline analysing Miseq/Miseq i100 outputs.
 UID=$(id -u) GID=$(id -g) docker compose -f $COMPOSE_YAML build
 ```
 
-###
+###　Perform programs
+There are three ways to perform programs.
+
+1. Run w/o entering to docker conteiner.
+e.g. Run Cutadapt.
+```
+docker compose -f $COMPOSE_YAML \
+  run --rm --user "$(id -u):$(id -g)" amplymate \
+  bash /data/_SCRIPTS/01_CUTADAPT.sh -f ${SCRIPT_PATH}/FWD.fasta -e ${CUTADAPT_eroor}
+```
+
+2. Run interactively.
+```
+docker compose -f $COMPOSE_YAML run --user "$(id -u):$(id -g)" --rm amplymate bash
+```
+
+3. Run on Rstudio server.
+```
+docker compose -f $COMPOSE_YAML up -d amplymate
+open http://localhost:8787/
+```
--- scripts/03_FILTERING.R
+++ scripts/03_FILTERING.R
@@ -1,11 +1,21 @@
 # %% CONFIGS
 MIN_LEN <- 100
-MAXEE <- 5
+MAXEE <- 2
 TRUNCLEN <- 0
+TRUNCLEN2 <- 0
 MINQ <- 10
 
 library(dada2)
 library(stringr)
+library(optparse)
+
+option_list <- list(
+  make_option("--minlen", type = "integer", default = "100"),
+  make_option("--maxee", type = "integer", default = 2),
+  make_option("--minq", type = "integer", default = 10),
+  make_option("--truncR1", type = "integer", default = 270),
+  make_option("--truncR2", type = "integer", default = 250),
+)
 
 INPUT <- Sys.getenv("TRIMMING")
 OUTPUT <- Sys.getenv("QUALITY_CONTROL")
@@ -33,7 +43,7 @@ if (length(file_names[[2]]) == 0) {
   out <- filterAndTrim(
     file_names[[1]], filtered_file[[1]],
     maxN = 0, rm.lowcomplex = 10, matchIDs = TRUE,
-    maxEE = MAXEE, minLen = MIN_LEN, truncLen = TRUNCLEN, minQ = MINQ, truncQ = MINQ,
+    maxEE = MAXEE, minLen = MIN_LEN, truncLen = c(TRUNCLEN, TRUNCLEN2), minQ = MINQ, truncQ = MINQ,
     rm.phix = TRUE, compress = TRUE, multithread = TRUE
   )
 } else {
