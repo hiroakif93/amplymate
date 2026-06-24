de0e98d522847eeef7d1aa1aa22c721e3df8cdcf
hiroakif <hiroakif1637@gmail.com>
2026-06-24 11:57:30 +0900

    update

--- scripts/05_ANNOTATION.R
+++ scripts/05_ANNOTATION.R
@@ -1,8 +1,8 @@
 # %% Configs
 
-REF_PATH="/db"
-REFERENCE = jsonlite::fromJSON(Sys.getenv("REF_DB"))[[1]]
-REFERENCE_ADD_SP="/db/silva_v138.2_assignSpecies.fa.gz"
+REF_PATH <- "/db"
+REFERENCE <- jsonlite::fromJSON(Sys.getenv("REF_DB"))[[1]]
+REFERENCE_ADD_SP <- "silva_v138.2_assignSpecies.fa.gz"
 
 # %% MAIN
 library(dada2)
@@ -10,48 +10,47 @@ library(seqinr)
 library(stringr)
 library(dplyr)
 
-INPUT=Sys.getenv("DENOISING")
-OUTPUT=Sys.getenv("ANNOTATION")
+INPUT <- Sys.getenv("DENOISING")
+OUTPUT <- Sys.getenv("ANNOTATION")
 
 dir.create(OUTPUT)
 
-seqtab = readRDS( sprintf("%s/seqtab_rmChimera.rds",INPUT))
-cols = colnames(seqtab)
-seqfasta = read.fasta(sprintf("%s/nonchim_seq.fasta",INPUT))
+seqtab <- readRDS(sprintf("%s/seqtab_rmChimera.rds", INPUT))
+cols <- colnames(seqtab)
+seqfasta <- read.fasta(sprintf("%s/nonchim_seq.fasta", INPUT))
 
-seqlist <- sapply(seqfasta, paste, collapse="")
-taxa <- assignTaxonomy(seqlist, paste0(REF_PATH, "/",REFERENCE), multithread=TRUE)
-if(str_detect(REFERENCE, "silva") & !all(colnames(taxa)=="Species")) taxa <- addSpecies(taxa, REFERENCE_ADD_SP)
+seqlist <- sapply(seqfasta, paste, collapse = "")
+taxa <- assignTaxonomy(seqlist, paste0(REF_PATH, "/", REFERENCE), multithread = TRUE)
+if (str_detect(REFERENCE, "silva") & !all(colnames(taxa) == "Species")) taxa <- addSpecies(taxa, paste0(REF_PATH, "/", REFERENCE_ADD_SP))
 
-taxa.print <- cbind(ASV=names(seqlist),taxa)  # Removing sequence rownames for display only
+taxa.print <- cbind(ASV = names(seqlist), taxa) # Removing sequence rownames for display only
 
-if( max( sapply(strsplit(taxa.print[,"Kingdom"], "k__"), length), na.rm=TRUE ) > 1){
-  taxa.print[,"Kingdom"] <- sapply(strsplit(taxa.print[,"Kingdom"], "k__"),  "[" ,2)
+if (max(sapply(strsplit(taxa.print[, "Kingdom"], "k__"), length), na.rm = TRUE) > 1) {
+  taxa.print[, "Kingdom"] <- sapply(strsplit(taxa.print[, "Kingdom"], "k__"), "[", 2)
 }
 
-taxa.print[,"Phylum"] <- gsub("p__", "", taxa.print[,"Phylum"])
-taxa.print[,"Class"] <- gsub("c__", "", taxa.print[,"Class"])
-taxa.print[,"Order"] <- gsub("o__", "", taxa.print[,"Order"])
-taxa.print[,"Family"] <- gsub("f__", "", taxa.print[,"Family"])
-taxa.print[,"Genus"] <- gsub("g__", "", taxa.print[,"Genus"])
-taxa.print[,"Species"] <- gsub("s__", "", taxa.print[,"Species"])
+taxa.print[, "Phylum"] <- gsub("p__", "", taxa.print[, "Phylum"])
+taxa.print[, "Class"] <- gsub("c__", "", taxa.print[, "Class"])
+taxa.print[, "Order"] <- gsub("o__", "", taxa.print[, "Order"])
+taxa.print[, "Family"] <- gsub("f__", "", taxa.print[, "Family"])
+taxa.print[, "Genus"] <- gsub("g__", "", taxa.print[, "Genus"])
+taxa.print[, "Species"] <- gsub("s__", "", taxa.print[, "Species"])
 
 taxa.print[is.na(taxa.print)] <- "Unidentified"
 taxa.print <- gsub("unidentified", "Unidentified", taxa.print)
-taxa.print = cbind(taxa.print, seq=rownames(taxa.print))
-rownames(taxa.print) = NULL
+taxa.print <- cbind(taxa.print, seq = rownames(taxa.print))
+rownames(taxa.print) <- NULL
 
-taxa_filterd = taxa.print|>
+taxa_filterd <- taxa.print |>
   as.data.frame() |>
   filter(
-    Phylum!="Unidentified",
-    Order!="Chloroplast", Family!="Mitochondria"
+    Phylum != "Unidentified",
+    Order != "Chloroplast", Family != "Mitochondria"
   )
 
 saveRDS(taxa.print, sprintf("%s/taxonomy_list.rds", OUTPUT))
 write.csv(taxa.print, sprintf("%s/taxonomy_list.csv", OUTPUT), row.names = FALSE)
 saveRDS(taxa_filterd, sprintf("%s/taxonomy_list_filt.rds", OUTPUT))
 
-id = pull(taxa_filterd, ASV)
-saveRDS(seqtab[,id], sprintf("%s/seqtab_filt.rds", OUTPUT))
-write.fasta(taxa_filterd$seq, taxa_filterd$ASV,  sprintf("%s/target_seq.fasta", OUTPUT))
+id <- pull(taxa_filterd, ASV)
+write.fasta(taxa_filterd$seq, taxa_filterd$ASV, sprintf("%s/target_seq.fasta", OUTPUT))
--- scripts/06_CLUSTERING_ASV.R
+++ scripts/06_CLUSTERING_ASV.R
@@ -1,21 +1,20 @@
-MIN_th=0.96
-MAX_th=1.00
-BY=0.02
-CLUSTER_TYPE="--cluster_size"
-target_taxa = c("Bacteria", "Archea", "Eukaryota")
+MIN_th <- 0.96
+MAX_th <- 1.00
+BY <- 0.02
+CLUSTER_TYPE <- "--cluster_size"
+target_taxa <- jsonlite::fromJSON(Sys.getenv("TARGET_TAXA"))[[1]]
 
 library(seqinr)
 library(purrr)
 library(dplyr)
 
-INPUT=Sys.getenv("DENOISING") #"$DENOISING/nonchim_seq.fasta"
-DENOIZING=Sys.getenv("DENOISING")
-ANNOTATION=Sys.getenv("ANNOTATION")
-OUTPUT=Sys.getenv("CLUSTERING") #"$CLUSTERING"
+DENOIZING <- Sys.getenv("DENOISING")
+ANNOTATION <- Sys.getenv("ANNOTATION")
+OUTPUT <- Sys.getenv("CLUSTERING") # "$CLUSTERING"
 
-seqtab = readRDS(sprintf("%s/seqtab_rmChimera.rds", DENOIZING))
-taxa_table = readRDS(sprintf("%s/taxonomy_list.rds", ANNOTATION))
-fasta = read.fasta(sprintf("%s/nonchim_seq.fasta", DENOIZING))
+seqtab <- readRDS(sprintf("%s/seqtab_rmChimera.rds", DENOIZING))
+taxa_table <- readRDS(sprintf("%s/taxonomy_list.rds", ANNOTATION))
+fasta <- read.fasta(sprintf("%s/nonchim_seq.fasta", DENOIZING))
 
 # %% sMAIN
 dir.create(OUTPUT)
@@ -23,12 +22,11 @@ dir.create(OUTPUT)
 ## VSEARCH
 
 ### Add abundance info to all fasta
-total_read_in_asv = colSums(seqtab)
-if(all(names(total_read_in_asv)==names(fasta))) names(fasta) = sprintf("%s;size=%s", names(fasta), total_read_in_asv)
-write.fasta(fasta, names(fasta), sprintf("%s/nonchim_seq_w_size.fasta", OUTPUT) )
+total_read_in_asv <- colSums(seqtab)
+if (all(names(total_read_in_asv) == names(fasta))) names(fasta) <- sprintf("%s;size=%s", names(fasta), total_read_in_asv)
+write.fasta(fasta, names(fasta), sprintf("%s/nonchim_seq_w_size.fasta", OUTPUT))
 
-for(id in seq(MIN_th, MAX_th, BY)){
-  
+for (id in seq(MIN_th, MAX_th, BY)) {
   system2(
     "vsearch",
     args = c(
@@ -41,37 +39,39 @@ for(id in seq(MIN_th, MAX_th, BY)){
 }
 
 ## MERGING
-merge_list = c()
-for(id in seq(MIN_th, MAX_th, BY)){
-  
-  asv_otu_tab = read.table(sprintf("%s/ASV_OTU_corestab_%s.txt", OUTPUT, id), header = TRUE)
-  
-  ### make table
-  num_asv_inotu = colSums(asv_otu_tab[,-c(1:3)]>0)
-  otu_col = map2(names(num_asv_inotu), num_asv_inotu, ~rep(.x, .y))|>unlist()
+merge_list <- c()
+for (id in seq(MIN_th, MAX_th, BY)) {
+  asv_otu_tab <- read.table(sprintf("%s/ASV_OTU_corestab_%s.txt", OUTPUT, id), header = TRUE)
 
-  asv = asv_otu_tab[,"Group"]
-  asv_id = apply(asv_otu_tab[,-c(1:3)], 2, \(x)which(x>0))
-  asv_col = sapply(asv_id, \(x)asv[x])|>unlist()
-  
+  ### make table
+  num_asv_inotu <- colSums(asv_otu_tab[, -c(1:3)] > 0)
+  otu_col <- map2(names(num_asv_inotu), num_asv_inotu, ~ rep(.x, .y)) |> unlist()
+
+  asv <- asv_otu_tab[, "Group"]
+  asv_id <- apply(asv_otu_tab[, -c(1:3)], 2, \(x)which(x > 0))
+  asv_col <- sapply(asv_id, \(x)asv[x]) |> unlist()
+
   id_label <- gsub("\\.", "", as.character(id))
-  tmp = tibble(ASV = asv_col, !!paste0("OTU_", id_label) := otu_col)
-  
+  tmp <- tibble(ASV = asv_col, !!paste0("OTU_", id_label) := otu_col)
+
   merge_list[[as.character(id)]] <- tmp
 }
 
-merged_table = reduce(merge_list, full_join, by = "ASV")|>
+merged_table <- reduce(merge_list, full_join, by = "ASV") |>
   arrange(ASV)
 
-taxa_table = merged_table|>
-  left_join(taxa_table|>as.data.frame(), by="ASV")
+taxa_table <- merged_table |>
+  left_join(taxa_table |> as.data.frame(), by = "ASV")
 
-taxa_table_filt = taxa_table|>
+taxa_table_filt <- taxa_table |>
   filter(
     Kingdom %in% target_taxa,
-    Phylum!="Unidentified",
-    Order!="Chloroplast", Family!="Mitochondria"
+    Phylum != "Unidentified",
+    Order != "Chloroplast", Family != "Mitochondria"
   )
 saveRDS(taxa_table, sprintf("%s/taxonomy_list.rds", OUTPUT))
 write.csv(taxa_table, sprintf("%s/taxonomy_list.csv", OUTPUT), row.names = FALSE)
 saveRDS(taxa_table_filt, sprintf("%s/taxonomy_list_filt.rds", OUTPUT))
+
+id <- as.character(taxa_table_filt$ASV)
+saveRDS(seqtab[, id], sprintf("%s/seqtab_filt.rds", OUTPUT))
